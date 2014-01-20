f77120b5cf9af80a328e892e1214812a50c7d618
ronasimi <ron.asimi@gmail.com>
2014-01-19 19:47:38 -0500

    Revert "Try low freqs again"
    This reverts commit d422fbee4ecda67ba7de047db6d80892091b9f03.

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3510,6 +3510,7 @@ static struct rcg_clk gfx2d1_clk = {
 		.ctl_val = CC_BANKED(9, 6, n), \
 	}
 
+#ifdef CONFIG_GPU_OVERCLOCK
 /*Shared by 8064, 8930, and 8960ab*/
 static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(        0, gnd,   0,  0),
@@ -3530,11 +3531,35 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(320000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
 	F_GFX3D(450000000, pll15, 1,  2),
-#ifdef CONFIG_GPU_OVERCLOCK
-	F_GFX3D(487500000, pll15, 1,  2),
-#endif
+    F_GFX3D(487500000, pll15, 1,  2),
 	F_END
 };
+
+#else
+
+/*Shared by 8064, 8930, and 8960ab*/
+static struct clk_freq_tbl clk_tbl_gfx3d[] = {
+	F_GFX3D(        0, gnd,   0,  0),
+	F_GFX3D( 27000000, pxo,   0,  0),
+	F_GFX3D( 48000000, pll8,  1,  8),
+	F_GFX3D( 54857000, pll8,  1,  7),
+	F_GFX3D( 64000000, pll8,  1,  6),
+	F_GFX3D( 76800000, pll8,  1,  5),
+	F_GFX3D( 96000000, pll8,  1,  4),
+	F_GFX3D(128000000, pll8,  1,  3),
+	F_GFX3D(145455000, pll2,  2, 11),
+	F_GFX3D(160000000, pll2,  1,  5),
+	F_GFX3D(177778000, pll2,  2,  9),
+	F_GFX3D(192000000, pll8,  1,  2),
+	F_GFX3D(200000000, pll2,  1,  4),
+	F_GFX3D(228571000, pll2,  2,  7),
+	F_GFX3D(266667000, pll2,  1,  3),
+	F_GFX3D(320000000, pll2,  2,  5),
+	F_GFX3D(400000000, pll2,  1,  2),
+	F_GFX3D(450000000, pll15, 1,  2),
+	F_END
+};
+#endif
 
 static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_GFX3D(        0, gnd,  0,  0),
@@ -3579,6 +3604,20 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930ab[] = {
 	F_END
 };
 
+#ifdef CONFIG_GPU_OVERCLOCK
+static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
+	[VDD_DIG_LOW]     = 128000000,
+	[VDD_DIG_NOMINAL] = 325000000,
+	[VDD_DIG_HIGH]    = 487500000
+};
+
+static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
+	[VDD_DIG_LOW]     = 128000000,
+	[VDD_DIG_NOMINAL] = 325000000,
+	[VDD_DIG_HIGH]    = 487500000
+};
+#else
+
 static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
@@ -3588,16 +3627,9 @@ static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 325000000,
-#ifdef CONFIG_GPU_OVERCLOCK
-#ifdef CONFIG_GPU_OVERCLOCK_450
-	[VDD_DIG_HIGH]    = 450000000
-#else
-	[VDD_DIG_HIGH]    = 487500000
-#endif
-#else
 	[VDD_DIG_HIGH]    = 400000000
-#endif
 };
+#endif
 
 static unsigned long fmax_gfx3d_8930[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 192000000,
@@ -6571,18 +6603,11 @@ static void __init reg_init(void)
 		if (!readl_relaxed(PRNG_CLK_NS_REG))
 			writel_relaxed(0x2B, PRNG_CLK_NS_REG);
 	}
-#ifdef CONFIG_GPU_OVERCLOCK_450
-	if (cpu_is_apq8064aa()) {
-#else
+
 	if (cpu_is_apq8064() || cpu_is_apq8064aa()) {
-#endif
 		/* Program PLL15 to 975MHz with ref clk = 27MHz */
 		configure_sr_pll(&pll15_config, &pll15_regs, 0);
-#ifdef CONFIG_GPU_OVERCLOCK_450
-	} else if (cpu_is_apq8064() || cpu_is_apq8064ab()) {
-#else
 	} else if (cpu_is_apq8064ab()) {
-#endif
 		/* Program PLL15 to 900MHZ */
 		pll15_config.l = 0x21 | BVAL(31, 7, 0x620);
 		pll15_config.m = 0x1;

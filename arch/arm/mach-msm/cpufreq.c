9f03726107959162c656466e1665998347942f64
Ron Asimi <ron.asimi@gmail.com>
2014-05-11 00:52:19 -0400

    derp: forgot to terminate an ifdef

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -221,6 +221,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
 		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
 #endif
+#endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
 #ifdef CONFIG_LOW_CPUCLOCKS
